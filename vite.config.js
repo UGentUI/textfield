384193ade0ef6e4601c278a08dfc79b908074403
kvachte <karel.vanachte@ugent.be>
2024-08-05 10:56:17 +0200

    Include dist folder for distribution

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,20 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/textfield/",
+  css: {
+    postcss: "./postcss.config.js",
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/textfield.js",
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === "index.css") {
+            return "assets/reset.css";
+          }
+          return "assets/[name]-[hash][extname]";
+        },
+      },
+    },
+  },
 });
